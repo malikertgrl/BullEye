1a6ac4fccfec46d3b89fdc99ac8d823e93b91f5b
malik ertuğrul <ertgrl.malik1@gmail.com>
2021-06-03 14:30:12 +0200

    first commit

---
+++ BullEye/aboutViewController.swift
@@ -0,0 +1,17 @@
+//
+//  aboutViewController.swift
+//  BullEye
+//
+//  Created by user194136 on 6/3/21.
+//
+
+import UIKit
+import WebKit
+
+class aboutViewController: UIViewController {
+    
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+}
